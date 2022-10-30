9fcce97d141ca6c0501ff0f92e967a12966bee97
yelosolutions <96409558+yelosolutions@users.noreply.github.com>
2022-10-30 14:16:48 +0300

    displaying objects on the index view

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,20 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username':'Joseph Rono'}
-    return render_template('index.html', title='home', user=user)
+    req_posts = [
+        {
+            'student': {'username': 'Joel'},
+            'body': 'I need an attachment in KCB bank'
+        },
+        {
+            'student': {'username': 'Soi'},
+            'body': 'I need an attachment in Kenyatta University'
+        }
+    ]
+    remark_posts = [
+        {
+            'lecturer': {'username': 'Mwangi'},
+            'body': 'Joel did a great work, He deserves an A'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, req_posts=req_posts, remark_posts=remark_posts)
