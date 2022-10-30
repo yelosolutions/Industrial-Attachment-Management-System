d53547f97e390f08ad8bed0eac7d5b299188484c
yelosolutions <96409558+yelosolutions@users.noreply.github.com>
2022-10-30 13:33:11 +0300

    rendering index view using index.html

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, world!"
+    user = {'username':'Joseph Rono'}
+    return render_template('index.html', title='home', user=user)
