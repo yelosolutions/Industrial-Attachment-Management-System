b83dd72103cd3a8a9eba250baa411a319b4420f9
yelosolutions <96409558+yelosolutions@users.noreply.github.com>
2022-10-31 18:13:01 +0300

    several changes

--- app/forms.py
+++ app/forms.py
@@ -5,7 +5,7 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    """ Class object represents the form 
+    """ Class object represents a login form
     """
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -30,5 +30,5 @@ def login():
     if form.validate_on_submit():
         flash('Validation required for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        return redirect('/index')
+        return redirect(url_for('/index'))
     return render_template('login.html', title="Sign In", form=form)
