3011f640701eefebbcebfa6b6e3487916d420291
yelosolutions <96409558+yelosolutions@users.noreply.github.com>
2022-10-31 04:32:05 +0300

    forms

---
+++ app/forms.py
@@ -0,0 +1,13 @@
+# import class FlaskForm from the package flask_wtf, an extension of flask
+# import classes that reprent form fields from wtforms 
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    """ Class object represents the form 
+    """
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Submit')
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -22,3 +23,12 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, req_posts=req_posts, remark_posts=remark_posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Validation required for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title="Sign In", form=form)
