1e2e13c3704baf5a80ec6d5fc395c5f01f9ad412
yelosolutions <96409558+yelosolutions@users.noreply.github.com>
2022-11-01 22:19:52 +0300

    more changes

--- app/routes.py
+++ app/routes.py
@@ -30,7 +30,7 @@ def login():
     if form.validate_on_submit():
         flash('Validation required for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        return redirect(url_for('/index'))
+        return redirect(url_for('index'))
     return render_template('login.html', title="Sign In", form=form)
 
 @app.route('/request', methods=['GET', 'POST'])
