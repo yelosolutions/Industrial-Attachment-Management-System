2f7f0460d46cef832909563cc7d5ab5714a24efe
yelosolutions <96409558+yelosolutions@users.noreply.github.com>
2022-11-02 18:53:39 +0300

    more commits

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,7 @@
 # import class FlaskForm from the package flask_wtf, an extension of flask
 # import classes that reprent form fields from wtforms 
-import email
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, DateTimeField, IntegerField
+from wtforms import StringField, SelectField, PasswordField, BooleanField, SubmitField, DateTimeField, IntegerField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
@@ -12,6 +11,15 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Submit')
+    
+class SignupForm(FlaskForm):
+    """ Class object represents a login form
+    """
+    user = SelectField(u'Select one', choices=['Student Number', 'Staff Number'])
+    usernumber = StringField('Student/Staff Number')
+    new_password = PasswordField('New Password')
+    confirm_password = PasswordField('Confirm Password')
+    register = SubmitField('Register')
 
 class RequestForm(FlaskForm):
     """ Class object represents a request form
@@ -20,4 +28,10 @@ class RequestForm(FlaskForm):
     email = StringField('Email Address', validators=[DataRequired()])
     contact = IntegerField('Student Phone Number')
     period = DateTimeField('Attachment Period')
+    submit = SubmitField('Submit')
+    
+class NamerForm(FlaskForm):
+    """ Class object represents a request form
+    """
+    name = StringField('Student Name', validators=[DataRequired()])
     submit = SubmitField('Submit')
--- app/routes.py
+++ app/routes.py
@@ -1,43 +1,43 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
-from app.forms import LoginForm, RequestForm
+from app.forms import LoginForm, RequestForm, NamerForm, SignupForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username':'Joseph Rono'}
-    req_posts = [
-        {
-            'student': {'username': 'Joel'},
-            'body': 'I need an attachment in KCB bank'
-        },
-        {
-            'student': {'username': 'Soi'},
-            'body': 'I need an attachment in Kenyatta University'
-        }
-    ]
-    remark_posts = [
-        {
-            'lecturer': {'username': 'Mwangi'},
-            'body': 'Joel did a great work, He deserves an A'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, req_posts=req_posts, remark_posts=remark_posts)
+    
+    return render_template('index.html', title='Home')
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/users/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Validation required for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        flash("Form Submited Successfully")
     return render_template('login.html', title="Sign In", form=form)
+
+@app.route('/users/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        flash("Form Submited Successfully")
+    return render_template('signup.html', title="Sign Up", form=form)
 
 @app.route('/request', methods=['GET', 'POST'])
 def request():
     form = RequestForm()
     if form.validate_on_submit():
-        flash('Validation required for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        flash('Request Form Submitted for {} '.format(
+            form.username.data))
         return redirect(url_for('/index'))
     return render_template('request.html', title="Request", form=form)
+
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash('Form Submitted Successfully')
+        #return redirect(url_for('/index'))
+    return render_template('name.html', title="Name", name=name, form=form)
