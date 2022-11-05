a70c3ca4c08279d0feed8079ab7f7085ccaadc05
yelosolutions <96409558+yelosolutions@users.noreply.github.com>
2022-11-05 12:11:47 +0300

    redirection

--- app/routes.py
+++ app/routes.py
@@ -1,29 +1,43 @@
 from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user, logout_user, login_required
 from app import app, db
 from app.models import User
 from app.forms import LoginForm, RequestForm, StudentForm, NamerForm, SignupForm
+from flask import request
+from werkzeug.urls import url_parse
+
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    
     return render_template('index.html', title='Home')
 
-@app.route('/users/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash("Form Submited Successfully")
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid username or password")
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title="Sign In", form=form)
 
-@app.route('/users/signup', methods=['GET', 'POST'])
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = SignupForm()
     if form.validate_on_submit():
         flash("Form Submited Successfully")
     return render_template('signup.html', title="Sign Up", form=form)
 
-@app.route('/users/add', methods=['GET', 'POST'])
+@app.route('/add', methods=['GET', 'POST'])
 def add_student():
     name = None
     form = StudentForm()
@@ -45,13 +59,13 @@ def add_student():
         students=students)
 
 @app.route('/request', methods=['GET', 'POST'])
-def request():
+def request_att():
     form = RequestForm()
     if form.validate_on_submit():
         flash('Request Form Submitted for {} '.format(
             form.username.data))
-        return redirect(url_for('/index'))
-    return render_template('request.html', title="Request", form=form)
+        return redirect(url_for('index'))
+    return render_template('request_att.html', title="Request", form=form)
 
 @app.route('/name', methods=['GET', 'POST'])
 def name():
@@ -63,3 +77,8 @@ def name():
         flash('Form Submitted Successfully')
         #return redirect(url_for('/index'))
     return render_template('name.html', title="Name", name=name, form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
